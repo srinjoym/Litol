f8055074b388546d3c5c1f3501ffc2ac80c16458
srinjoym <srinjoy.majumdar@gmail.com>
2015-07-13 13:19:56 -0700

    finished chapter indicator for users

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,4 +1,5 @@
 class Course < ActiveRecord::Base
  has_many :chapters
+ has_many :quiz_results
  has_and_belongs_to_many :organizations
 end
--- app/models/quiz_result.rb
+++ app/models/quiz_result.rb
@@ -1,4 +1,5 @@
 class QuizResult < ActiveRecord::Base
   belongs_to :quiz
   belongs_to :user
+  belongs_to :chapter
 end
