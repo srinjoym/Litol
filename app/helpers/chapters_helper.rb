464375ed79c65de99a568cc4f312f7a8a83f2827
srinjoym <srinjoy.majumdar@gmail.com>
2015-07-19 22:51:59 -0700

    added chapter link to section view

--- app/helpers/chapters_helper.rb
+++ app/helpers/chapters_helper.rb
@@ -1,2 +1,15 @@
 module ChaptersHelper
+  def passed? (chapter)
+    if !chapter.quiz.nil? && !chapter.quiz.quiz_results.find_by(user_id: current_user.id).nil?
+      if chapter.quiz.quiz_results.find_by(user_id: current_user.id, passed: false)
+        return 0
+      elsif chapter.quiz.quiz_results.find_by(user_id: current_user.id, passed: true)
+        return 1
+      end
+    else
+      return -1
+    end
+  end
 end
+
+
