edf3f8d7749e6f25bb4a5b4c6d89034c083dfaed
srinjoym <srinjoy.majumdar@gmail.com>
2015-07-28 15:32:05 -0700

    fixed image upload bug

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -42,7 +42,7 @@ class CoursesController < ApplicationController
         chapter.sections = @oldCourse.chapters.find_by(id: chapter.id).sections
       end
       @newCourse.default=false
-      if @newCourse.imageSource.nil?
+      if @newCourse.photo.nil?
         @newCourse.imageSource=@oldCourse.imageSource
       end
       @newCourse.save
